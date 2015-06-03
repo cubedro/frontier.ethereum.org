ef6b5e7578568021e38e5f369ed74ad7883736c9
Alexandre Van de Sande <alex.vandesande@ethdev.com>
2015-06-03 17:44:54 -0300

    I should probably have started committing a long time ago..

--- public/js/script.js
+++ public/js/script.js
@@ -1,26 +1,47 @@
-(function(){
-	hljs.initHighlightingOnLoad();
+function isElementInViewport (el) {
 
-	$('#shell-source').hide().removeClass('hidden');
-	$('#install-info').hide().removeClass('hidden');
+    //special bonus for those using jQuery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
 
-	$('#show-source').on('click', function() {
-		$('#shell-source').show();
-		$(this).hide();
-	});
+    var rect = el.getBoundingClientRect();
 
-	$('#hide-source').on('click', function() {
-		$('#show-source').show();
-		$('#shell-source').hide();
-	});
+    return (
+        rect.bottom >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) 
+    );
+}
 
-	$('#understand').change(function() {
-		$('#show-install').toggleClass('disabled', !$(this).prop( "checked" ));
-	});
+function onVisibilityChange (el, callback) {
+    return function () {
+        /*your code here*/ console.log('visibility: ' + isElementInViewport(el));
+        //for(var i = 1; i < 3; i++) {
+	  //       if (isElementInViewport($("div.main-tutorial.part" + i))) {
+			// 	$("ul#toc > li:nth-child("+i+")").show();
+			// } else {
+			// 	$("ul#toc > li:nth-child("+i+")").hide();
+			// }        	
+        //}
 
-	$('#show-install').on('click', function() {
-		$('#install-info').show();
-		$('#install-terms').hide();
-	});
-})();
+    	if (isElementInViewport($("div.main-tutorial.part1"))) {
+			$("ul#toc > li:nth-child(1) ul").show(400);
+		} else {
+			$("ul#toc > li:nth-child(1) ul").hide(400);
+		} 
+
+    	if (isElementInViewport($("div.main-tutorial.part2"))) {
+			$("ul#toc > li:nth-child(2) ul").show(400);
+		} else {
+			$("ul#toc > li:nth-child(2) ul").hide(400);
+		} 
+
+    	if (isElementInViewport($("div.main-tutorial.part3"))) {
+			$("ul#toc > li:nth-child(3) ul").show(400);
+		} else {
+			$("ul#toc > li:nth-child(3) ul").hide(400);
+		}  
+
+    }
+}
 
